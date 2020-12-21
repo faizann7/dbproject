8305e14e70c192978d303c81512509f10317e9d2
Faizan <mohammad.faizan6th@gmail.com>
2020-12-21 20:11:54 +0500

    some working

--- commerce/commerce/settings.py
+++ commerce/commerce/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'commerce',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,8 @@ USE_TZ = True
 
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+# added manually
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
