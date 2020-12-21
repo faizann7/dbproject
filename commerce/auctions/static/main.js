832ab63684e4fb76cd3aefebb2b05a032a9b7517
Faizan <mohammad.faizan6th@gmail.com>
2020-12-22 00:25:37 +0500

    login sign up

--- commerce/auctions/static/main.js
+++ commerce/auctions/static/main.js
@@ -25,26 +25,7 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 //scroll section active link
 
-sections = document.querySelectorAll('section[id]')
 
-window.addEventListener('scroll', scrollActive)
-
-function scrollActive(){
-    const scrollY = window.pageYOffset
-
-    sections.forEach(current => {
-        const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 50
-        sectionId = current.getAttribute('id')
-
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
-        }
-        else{
-            document.querySelector('.nav__menu a[href *=' + sectionId + ']').classList.remove('active')
-        }
-    })
-}
 
 
 
@@ -54,3 +35,9 @@ window.onscroll = () =>{
     if(this.scrollY >= 200 ) nav.classList.add('scroll-header');
     else nav.classList.remove('scroll-header')
 }
+
+
+
+
+// LOGIN
+/*===== LOGIN SHOW and HIDDEN =====*/
